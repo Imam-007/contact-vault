c39f78edf926fda0129f10018e9fb8f7ddea461a
Imam <imam@IBEEs-MacBook-Air-2.local>
2024-11-28 21:20:27 +0530

    initialized project

---
+++ src/test/java/com/imam/scm/ContactVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imam.scm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
